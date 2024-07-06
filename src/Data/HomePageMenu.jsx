57f740788596208bddc2501731d4d1e46bd8204a
Ayush <142646578+AyushAyuu@users.noreply.github.com>
2024-07-07 02:35:03 +0530

    all image

--- src/Data/HomePageMenu.jsx
+++ src/Data/HomePageMenu.jsx
@@ -3,117 +3,133 @@ const HomePageMenu = [
     id: 1,
     title: "पासपोर्ट फोटो",
     link: "https://photogov.com/documents/in-passport-35x45mm-photo/",
-    image: "./src/assets/images/photo.png",
+    image: "",
   },
   {
     id: 2,
     title: "समग्र परिवार आईडी",
     link: "https://samagra.gov.in/Public/Dashboard/SamagraProfile.aspx?fw=sf",
-    image: "./src/assets/images/samagra.png",
+    image:
+      "https://play-lh.googleusercontent.com/xDVKSsEK7IVn4ctNmgrY3pBOiFvrD4JybF-CUOmPnzyPLlAH-zOJuV-glcNzUemnEqw",
   },
   {
     id: 3,
     title: "समग्र सदस्य आईडी",
     link: "https://samagra.gov.in/Public/Dashboard/SamagraSearch.aspx",
-    image: "./src/assets/images/samagra.png",
+    image:
+      "https://play-lh.googleusercontent.com/xDVKSsEK7IVn4ctNmgrY3pBOiFvrD4JybF-CUOmPnzyPLlAH-zOJuV-glcNzUemnEqw",
   },
   {
     id: 4,
     title: "आधार कार्ड",
     link: "https://myaadhaar.uidai.gov.in/genricDownloadAadhaar/en",
     image:
-      "https://images.hindustantimes.com/tech/img/2022/12/19/960x540/aadhaar-card-7579588_960_720_1671442149571_1671442199832_1671442199832.webp",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6IwoZHwdqT1fPKQ74CW2YPkWo2B-1rWeG-g&s",
   },
   {
     id: 5,
     title: "पेन कार्ड",
     link: "https://www.pan.utiitsl.com/PAN_ONLINE/ePANCheckCard.action",
-    image: "./src/assets/images/pan.png",
+    image:
+      "https://5.imimg.com/data5/SELLER/Default/2021/5/AC/UD/AP/83660139/pan-card-services-500x500.jpg",
   },
   {
     id: 6,
     title: "आयुष्मान कार्ड",
     link: "https://beneficiary.nha.gov.in/",
-    image: "./src/assets/images/ayushman.png",
+    image:
+      "https://play.google.com/store/apps/details?id=com.beneficiaryapp&hl=en_US",
   },
   {
     id: 7,
     title: "आय प्रमाण-पत्र",
     link: "https://mpedistrict.gov.in/MPL/index.aspx",
-    image: "./src/assets/images/aaye.png",
+    image:
+      "https://cdn.s3waas.gov.in/s337a749d808e46495a8da1e5352d03cae/uploads/2019/06/2019062877.jpg",
   },
   {
     id: 8,
     title: "मूल्यवासी प्रमाण-पत्र",
     link: "https://mpedistrict.gov.in/MPL/index.aspx",
-    image: "./src/assets/images/mulyavasi.png",
+    image:
+      ".https://cdn.s3waas.gov.in/s337a749d808e46495a8da1e5352d03cae/uploads/2019/06/2019062877.jpg",
   },
   {
     id: 9,
     title: "जाति प्रमाण-पत्र",
     link: "https://mpedistrict.gov.in/MPL/index.aspx",
-    image: "./src/assets/images/jaati.png",
+    image:
+      "https://cdn.s3waas.gov.in/s337a749d808e46495a8da1e5352d03cae/uploads/2019/06/2019062877.jpg",
   },
   {
     id: 10,
     title: "खसरा-खतौनी-नक्शा ",
     link: "https://mpbhulekh.gov.in/mpbhulekh.do",
-    image: "./src/assets/images/naksha.png",
+    image: "https://mpbhulekh.gov.in/images/img/logo.png",
   },
   {
     id: 11,
     title: "जीवाजी रिजल्ट",
     link: "https://univindia.com/JwgResult/adselcolclassnew.php",
-    image: "./src/assets/images/jiwaji.png",
+    image:
+      "https://akm-img-a-in.tosshub.com/sites/resources/campus/prod/img/logo/2023/9/08122020jiwajimonocopy1533946756127.jpg",
   },
   {
     id: 12,
     title: "जीवाजी एडमिट-कार्ड",
     link: "https://jiwaji.mponline.gov.in/portal/Services/Jiwaji/exam/orguser/admit_card.aspx",
-    image: "./src/assets/images/jiwaji.png",
+    image:
+      "https://akm-img-a-in.tosshub.com/sites/resources/campus/prod/img/logo/2023/9/08122020jiwajimonocopy1533946756127.jpg",
   },
   {
     id: 13,
     title: "एमपी बोर्ड कक्षा 5 और 8 रिजल्ट",
     link: "https://www.rskmp.in/BoardExam/Result24/StudentResult.aspx",
-    image: "./src/assets/images/mpboard.png",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/d/d5/Board_of_Secondary_Education%2C_Madhya_Pradesh_logo.jpg",
   },
   {
     id: 14,
     title: "MP बोर्ड कक्षा 10 रिजल्ट",
     link: "https://mpresults.nic.in/mpbse/Xhsc-24/10th_class_24.htm",
-    image: "./src/assets/images/mpboard.png",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/d/d5/Board_of_Secondary_Education%2C_Madhya_Pradesh_logo.jpg",
   },
   {
     id: 15,
     title: "MP बोर्ड कक्षा 12 रिजल्ट",
     link: "https://mpresults.nic.in/mpbse/HSSC12_24/12th_2024-XII.htm",
-    image: "/assets/images/mpboard.png",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/d/d5/Board_of_Secondary_Education%2C_Madhya_Pradesh_logo.jpg",
   },
 
   {
     id: 16,
     title: "CBSC बोर्ड कक्षा 10 रिजल्ट",
     link: "https://cbseresults.nic.in/2024/CBSE10th/CBSE10thLogin?resultType=cbse10",
-    image: "./src/assets/images/cbsc.png",
+    image:
+      ".https://e7.pngegg.com/pngimages/612/865/png-clipart-central-board-of-secondary-education-ugc-net-cbse-exam-class-10-neet-jee-main-school-label-logo-thumbnail.png",
   },
   {
     id: 17,
     title: "CBSC बोर्ड कक्षा 12 रिजल्ट",
     link: "https://cbseresults.nic.in/2024/CBSE12th/CBSE12thLogin?resultType=cbse12",
-    image: "./src/assets/images/cbsc.png",
+    image:
+      ".https://e7.pngegg.com/pngimages/612/865/png-clipart-central-board-of-secondary-education-ugc-net-cbse-exam-class-10-neet-jee-main-school-label-logo-thumbnail.png",
   },
   {
     id: 18,
     title: "RGPV रिजल्ट",
     link: "http://result.rgpv.ac.in/Result/ProgramSelect.aspx",
-    image: "./src/assets/images/rgpv.png",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FRajiv_Gandhi_Proudyogiki_Vishwavidyalaya&psig=AOvVaw04sLu1wiZSYQ0DMDDRMTXn&ust=1720386242526000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCICM0qiok4cDFQAAAAAdAAAAABAE",
   },
   {
     id: 19,
     title: "सरकारी एडमिट कार्ड",
     link: "https://www.sarkariresult.com/admitcard/",
-    image: "./src/assets/images/sarkari.png",
+    image:
+      "https://play-lh.googleusercontent.com/vhu946egoixCAkwanZsVe0gYHcJ4Z4lTS-1rPA-mGnlZKax3X_Ncru4pRYwUGMvNuQU",
   },
 
   // bg remove , blur image
