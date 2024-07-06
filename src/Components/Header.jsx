c257a1c4c56011765b3b383bf1b8341e1ace547f
Ayush <142646578+AyushAyuu@users.noreply.github.com>
2024-07-07 04:02:57 +0530

    header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
               src="https://static.vecteezy.com/system/resources/previews/019/514/640/original/letter-v-logo-design-for-business-and-company-identity-with-luxury-concept-free-vector.jpg"
               alt=""
             /> */}
-            <h4 className="my-auto">वर्धमान फोटोकॉपी</h4>
+            <h4 className=" text-warning my-auto">वर्धमान फोटोकॉपी</h4>
           </div>
         </div>
       </nav>
